120fce76617500cdfba348ddc025d7918cf4ecb5
alexguirre <alexguirre.aa@gmail.com>
2016-10-07 20:45:51 +0200

    Add support for game patch v877

---
+++ src%20-%20C%23/Emergency%20Strobes%20CSharp/Extensions.cs
@@ -0,0 +1,75 @@
+﻿namespace Emergency_Strobes
+{
+    // System
+    using System.Drawing;
+
+    // RPH
+    using Rage;
+    using Rage.Native;
+
+    internal static class Extensions
+    {
+        public static unsafe void SetLeftHeadlightBroken(this Vehicle v, bool broken)
+        {
+            byte mask = 1 << 0;
+
+            if (broken)
+            {
+                *(int*)(v.MemoryAddress.ToInt64() + 0x079C) |= mask;
+            }
+            else
+            {
+                *(int*)(v.MemoryAddress.ToInt64() + 0x079C) &= ~mask;
+            }
+        }
+
+        public static unsafe void SetRightHeadlightBroken(this Vehicle v, bool broken)
+        {
+            byte mask = 1 << 1;
+
+            if (broken)
+            {
+                *(int*)(v.MemoryAddress.ToInt64() + 0x079C) |= mask;
+            }
+            else
+            {
+                *(int*)(v.MemoryAddress.ToInt64() + 0x079C) &= ~mask;
+            }
+        }
+
+        public static bool IsLeftHeadlightBroken(this Vehicle v)
+        {
+            return NativeFunction.Natives.GetIsLeftVehicleHeadlightDamaged<bool>(v);
+        }
+
+        public static bool IsRightHeadlightBroken(this Vehicle v)
+        {
+            return NativeFunction.Natives.GetIsRightVehicleHeadlightDamaged<bool>(v);
+        }
+
+        public static RectangleF ConvertToCurrentCoordSystem(this RectangleF rectangle)
+        {
+            Size origRes = Game.Resolution;
+            float aspectRatio = origRes.Width / (float)origRes.Height;
+            PointF pos = new PointF(rectangle.X / (1080 * aspectRatio), rectangle.Y / 1080f);
+            SizeF siz = new SizeF(rectangle.Width / (1080 * aspectRatio), rectangle.Height / 1080f);
+            return new RectangleF(pos.X * Game.Resolution.Width, pos.Y * Game.Resolution.Height, siz.Width * Game.Resolution.Width, siz.Height * Game.Resolution.Height);
+        }
+
+        public static Vector3 GetDeformationAt(this Vehicle v, Vector3 offset)
+        {
+            return NativeFunction.Natives.GetVehicleDeformationAtPos<Vector3>(v, offset.X, offset.Y, offset.Z);
+        }
+
+        //public static unsafe void SetLightMultiplier(this Vehicle v, float multiplier)
+        //{
+        //    //NativeFunction.Natives.SetVehicleLightMultiplier(v, multiplier);
+        //    *(float*)(v.MemoryAddress + 0x092C) = multiplier;
+        //}
+
+        //public static unsafe float GetLightMultiplier(this Vehicle v)
+        //{
+        //    return *(float*)(v.MemoryAddress + 0x092C);
+        //}
+    }
+}
